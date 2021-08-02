6614591301ffc5c4b76d9e4aecc82519fadebc35
octanium91 <octanium91@gmail.com>
2021-08-02 16:10:45 +0300

    add ga

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,15 @@
-import React, { Suspense } from "react";
+import React, {Suspense, useEffect} from "react";
+import ReactGA from 'react-ga';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { routes } from "./routes";
 import Loading from "./components/suspenseLoding";
 
 function App() {
+
+  useEffect(() => {
+      ReactGA.initialize('G-8X3LJW08X7');
+      ReactGA.pageview(window.location.pathname + window.location.search);
+  }, [])
 
   return (
       <Suspense fallback={<Loading/>}>

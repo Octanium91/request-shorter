f852c0e44ef2c5cef36a39331eeffb7f93317687
octanium91 <octanium91@gmail.com>
2021-08-02 16:26:14 +0300

    remove ga module

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,9 @@
 import React, {Suspense, useEffect} from "react";
-import ReactGA from 'react-ga';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { routes } from "./routes";
 import Loading from "./components/suspenseLoding";
 
 function App() {
-
-  useEffect(() => {
-      ReactGA.initialize('G-8X3LJW08X7');
-      ReactGA.pageview(window.location.pathname + window.location.search);
-  }, [])
 
   return (
       <Suspense fallback={<Loading/>}>

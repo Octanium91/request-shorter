5322b1e70fe635e3ac9e55bc0759c547fb23c4a9
octanium91 <octanium91@gmail.com>
2021-08-02 15:36:11 +0300

    add github card

--- front-end/src/components/footer/index.js
+++ front-end/src/components/footer/index.js
@@ -1,4 +1,5 @@
 import React, { Component }  from "react";
+import GitHubButton from 'react-github-btn'
 import { Card, CardContent, Typography, Link } from '@material-ui/core';
 import "./index.css"
 
@@ -15,6 +16,36 @@ class Footer extends Component {
                     <CardContent>
                         <Typography>Developed by Octanium91</Typography>
                         <Typography><b>Website: </b><Link href="https://octanium91.github.io/" rel="noreferrer" target="_blank">octanium91.github.io</Link></Typography>
+                    </CardContent>
+                </Card>
+                <Card style={{ marginTop: "15px", marginRight: "5px", marginLeft: "5px" }}>
+                    <CardContent>
+                        <Typography className={"d-flex  justify-content-center"}><b>GitHub</b></Typography>
+                        <div className={"d-flex  justify-content-center"}>
+                            <div style={{margin: "5px"}}>
+                                <GitHubButton href="https://github.com/Octanium91/request-shorter" data-size="large"
+                                              aria-label="Source Code">Source Code</GitHubButton>
+                            </div>
+                            <div style={{margin: "5px"}}>
+                                <GitHubButton href="https://github.com/octanium91" data-size="large"
+                                              aria-label="Follow @octanium91 on GitHub">Follow</GitHubButton>
+                            </div>
+                            <div style={{margin: "5px"}}>
+                                <GitHubButton href="https://github.com/octanium91/request-shorter/subscription"
+                                              data-icon="octicon-eye" data-size="large"
+                                              aria-label="Watch octanium91/request-shorter on GitHub">Watch</GitHubButton>
+                            </div>
+                            <div style={{margin: "5px"}}>
+                                <GitHubButton href="https://github.com/octanium91/request-shorter"
+                                              data-icon="octicon-star" data-size="large"
+                                              aria-label="Star octanium91/request-shorter on GitHub">Star</GitHubButton>
+                            </div>
+                            <div style={{margin: "5px"}}>
+                                <GitHubButton href="https://github.com/sponsors/octanium91" data-icon="octicon-heart"
+                                              data-size="large"
+                                              aria-label="Sponsor @octanium91 on GitHub">Sponsor</GitHubButton>
+                            </div>
+                        </div>
                     </CardContent>
                 </Card>
             </>

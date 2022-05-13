fa7533410adc014a4be1dd146cf6ffd788d42974
octanium91 <octanium91@gmail.com>
2022-05-13 23:27:03 +0300

    update nginx #3

--- infrastructure/gateway/start-nginx.sh
+++ infrastructure/gateway/start-nginx.sh
@@ -2,9 +2,9 @@
 
 echo "## START NGINX APP"
 envsubst "`printf '${%s} ' $(bash -c "compgen -A variable")`" < /etc/nginx/conf.d/conf.nginx > /etc/nginx/conf.d/default.conf
-exec /etc/nginx/reload.sh &
-exec nginx
+#exec /etc/nginx/reload.sh &
+#exec nginx
 #echo "## START WHILE"
 #while :; do sleep 6h & wait $${!}; nginx -s reload; done & nginx -g \"daemon off;\"
 #echo "## SCRIPT END"
-#/bin/sh -c 'while :; do sleep 6h & wait $${!}; nginx -s reload; done & nginx -g \"daemon off;\"'
+/bin/sh -c 'while :; do sleep 1m & wait $${!}; nginx -s reload; done & nginx -g \"daemon off;\"'

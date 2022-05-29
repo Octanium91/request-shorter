24a27d0c9d397b298701becd843dd9b39bda380f
octanium91 <octanium91@gmail.com>
2022-05-29 12:37:16 +0300

    add cert for stspznv.men

--- infrastructure/gateway/init-letsencrypt.sh
+++ infrastructure/gateway/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(slink.sbs portainer.slink.sbs)
+domains=(slink.sbs portainer.slink.sbs stspznv.men)
 rsa_key_size=4096
 data_path="../certbot"
 email="" # Adding a valid address is strongly recommended
